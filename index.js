1af68a40fbe04f788cf965ff2e29c45396b079b3
Daniel Lee <dleetrident@hotmail.com>
2021-07-01 12:40:29 +0100

    this is the real update, not previous one

--- index.js
+++ index.js
@@ -15,31 +15,30 @@ let chickenObj = new Ingredient(126,27,0,2,0.50)
 let steakObj = new Ingredient(217,26,0,12,2.00)
 let lambObj = new Ingredient(294,25,0,21,1.00)
 
+const ingredients = new Map()
+.set("lamb",lambObj)
+.set("steak",steakObj)
+.set("chicken",chickenObj)
+
 for(i=0;i<rangeOfIngredients.length;i++){
   document.querySelectorAll(".ingredient")[i].addEventListener("click",function(e){
     let imageClicked = e.target.id
     displayImage(imageClicked);
-
-    })
+   })
 }
 
 function displayImage(image){
 let display = document.querySelector(".display-ingredient")
-switch(image){
-  case "lamb":
-  display.src = "images/" + image + ".png";
-  lambObj.clicked = !lambObj.clicked
-  lambObj.clicked === true ? meal.push(lambObj) : meal.splice(meal.indexOf(lambObj),1)
-  break;
-  case "steak":
-  display.src = "images/" + image + ".png";
-  steakObj.clicked = !steakObj.clicked
-  steakObj.clicked === true ? meal.push(steakObj) : meal.splice(meal.indexOf(steakObj),1)
-  break;
-  case "chicken":
-  display.src = "images/" + image + ".png";
-  chickenObj.clicked = !chickenObj.clicked
-  chickenObj.clicked === true ? meal.push(chickenObj) : meal.splice(meal.indexOf(chickenObj),1)
-  break;
+if(ingredients.has(image)){
+  display.src = "images/" + image + ".png"
+  let obj = ingredients.get(image)
+  obj.clicked = !obj.clicked
+  obj.clicked === true ? meal.push(obj) : meal.splice(meal.indexOf(obj),1)
 }
 }
+
+document.querySelector("calculate-meal").addEventListener("click",function(){
+  for(i=0;i<meal.length;i++){
+
+  }
+})
